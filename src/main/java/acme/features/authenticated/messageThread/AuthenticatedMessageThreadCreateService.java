e4e5978278bf14d2f2483ec1d528dcf5d54b2675
alejandromol98 <alejandromol98@gmail.com>
2019-12-17 01:27:44 +0100

    MessageThread and message task added

---
+++ src/main/java/acme/features/authenticated/messageThread/AuthenticatedMessageThreadCreateService.java
@@ -0,0 +1,76 @@
+
+package acme.features.authenticated.messageThread;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.messageThreads.MessageThread;
+import acme.framework.components.Errors;
+import acme.framework.components.Model;
+import acme.framework.components.Request;
+import acme.framework.entities.Authenticated;
+import acme.framework.services.AbstractCreateService;
+
+@Service
+public class AuthenticatedMessageThreadCreateService implements AbstractCreateService<Authenticated, MessageThread> {
+
+	@Autowired
+	private AuthenticatedMessageThreadRepository repository;
+
+
+	@Override
+	public boolean authorise(final Request<MessageThread> request) {
+		assert request != null;
+		return true;
+	}
+
+	@Override
+	public void bind(final Request<MessageThread> request, final MessageThread entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+
+		request.bind(entity, errors, "moment");
+
+	}
+
+	@Override
+	public void unbind(final Request<MessageThread> request, final MessageThread entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "title", "members");
+
+	}
+
+	@Override
+	public MessageThread instantiate(final Request<MessageThread> request) {
+		MessageThread result;
+
+		result = new MessageThread();
+
+		return result;
+	}
+
+	@Override
+	public void validate(final Request<MessageThread> request, final MessageThread entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+
+	}
+
+	@Override
+	public void create(final Request<MessageThread> request, final MessageThread entity) {
+		Date moment;
+
+		moment = new Date(System.currentTimeMillis() - 1);
+		entity.setMoment(moment);
+		this.repository.save(entity);
+
+	}
+
+}
